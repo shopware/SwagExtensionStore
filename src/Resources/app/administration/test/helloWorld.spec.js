a3a8f95f65f3701afdd78de77d1bea599339d5d1
Jannis Leifeld <j.leifeld@shopware.com>
2021-02-16 11:30:54 +0100

    NEXT-13468 - add tests for new pages

--- src/Resources/app/administration/test/helloWorld.spec.js
+++
@@ -1,7 +0,0 @@
-import helloWorld from 'SwagExtensionStore/helloWorld';
-
-describe('testing the hello world', () => {
-    test('helloWorld returns "Hello World"', () => {
-        expect(helloWorld()).toEqual('Hello World');
-    });
-});
