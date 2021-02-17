a63ca37b006dd49d02841b1e505fc72efc48d72b
Jannis Leifeld <j.leifeld@shopware.com>
2021-02-17 13:14:53 +0100

    NEXT-13452 - change development branch to trunk

--- src/Resources/app/administration/build/webpack.config.js
+++ src/Resources/app/administration/build/webpack.config.js
@@ -2,9 +2,6 @@ const path = require('path');
 
 module.exports = () => {
     return {
-        optimization: {
-            moduleIds: 'named',
-        },
         resolve: {
             alias: {
                 SwagExtensionStore: path.join(__dirname, '..', 'src')
