3ffbe1e573900a5974f79c8821a2b98b92010f08
Sebastian Franze <s.franze@shopware.com>
2022-01-05 09:57:35 +0000

    EXS-69 - Use TestBootstrapper

--- tests/TestBootstrap.php
+++ tests/TestBootstrap.php
@@ -5,49 +5,13 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+use Shopware\Core\TestBootstrapper;
 
-use Shopware\Core\Framework\Test\TestCaseBase\KernelLifecycleManager;
-use Symfony\Component\Dotenv\Dotenv;
+$bootstrapper = new TestBootstrapper();
 
-function getProjectDir(): string
-{
-    if (isset($_SERVER['PROJECT_ROOT']) && \file_exists($_SERVER['PROJECT_ROOT'])) {
-        return $_SERVER['PROJECT_ROOT'];
-    }
-    if (isset($_ENV['PROJECT_ROOT']) && \file_exists($_ENV['PROJECT_ROOT'])) {
-        return $_ENV['PROJECT_ROOT'];
-    }
+$bootstrapper->getClassLoader()->addPsr4('SwagExtensionStore\\Tests\\', __DIR__);
 
-    $rootDir = __DIR__;
-    $dir = $rootDir;
-    while (!\file_exists($dir . '/.env')) {
-        if ($dir === \dirname($dir)) {
-            return $rootDir;
-        }
-        $dir = \dirname($dir);
-    }
-
-    return $dir;
-}
-
-require __DIR__ . '/KernelLifecycleManager.php';
-
-$testProjectDir = getProjectDir();
-
-/** @var \Composer\Autoload\ClassLoader $loader */
-$loader = require $testProjectDir . '/vendor/autoload.php';
-KernelLifecycleManager::prepare($loader);
-$loader->addPsr4('SwagExtensionStore\\Tests\\', __DIR__);
-$loader->addPsr4('SwagExtensionStore\\', dirname(__DIR__) . '/src');
-
-define('TEST_PROJECT_DIR', __DIR__);
-
-if (!\class_exists(Dotenv::class)) {
-    throw new RuntimeException('APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.');
-}
-(new Dotenv(true))->load($testProjectDir . '/.env');
-
-$dbUrl = \getenv('DATABASE_URL');
-if ($dbUrl !== false) {
-    \putenv('DATABASE_URL=' . $dbUrl . '_test');
-}
+$bootstrapper
+    ->setPlatformEmbedded(false)
+    ->addCallingPlugin()
+    ->bootstrap();
