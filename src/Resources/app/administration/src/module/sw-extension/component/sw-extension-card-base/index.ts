8bb09050a00710c9f91ea4c26ff288fbb8b5dc26
Albert Scherman <1457444+Bird87ZA@users.noreply.github.com>
2025-05-08 06:00:48 -0700

    feat: added available iaps to extensions (#75)

--- src/Resources/app/administration/src/module/sw-extension/component/sw-extension-card-base/index.ts
+++ src/Resources/app/administration/src/module/sw-extension/component/sw-extension-card-base/index.ts
@@ -1,3 +1,4 @@
+import type * as IAP from 'SwagExtensionStore/module/sw-in-app-purchases/types';
 import template from './sw-extension-card-base.html.twig';
 import './sw-extension-card-base.scss';
 
@@ -9,6 +10,17 @@ const accountUrl = 'https://account.shopware.com';
 export default Shopware.Component.wrapComponentConfig({
     template: template,
 
+    inject: [
+        'inAppPurchasesService'
+    ],
+
+    data() {
+        return {
+            inAppPurchases: [] as IAP.InAppPurchase[],
+            showInAppPurchasesListingModal: false
+        };
+    },
+
     methods: {
         openAccountPage() {
             window.open(`${accountUrl}/shops/shops`, '_blank');
@@ -16,6 +28,20 @@ export default Shopware.Component.wrapComponentConfig({
 
         hasActiveInAppPurchases(extensionName: string) {
             return Shopware.InAppPurchase.getByExtension(extensionName).length > 0;
+        },
+
+        openInAppPurchasesListingModal() {
+            this.showInAppPurchasesListingModal = true;
+            this.fetchInAppPurchases();
+        },
+
+        closeInAppPurchasesListingModal() {
+            this.showInAppPurchasesListingModal = false;
+            this.inAppPurchases = [];
+        },
+
+        async fetchInAppPurchases() {
+            this.inAppPurchases = await this.inAppPurchasesService.getAvailablePurchases((this.extension as IAP.Extension).name);
         }
     }
 });
