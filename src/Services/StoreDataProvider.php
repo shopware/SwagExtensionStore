935d854aedb56cb8a32f735b387e057349b101b3
Michel Bade <m.bade@shopware.com>
2025-05-13 13:18:16 +0200

    fix: header case sensititvity

--- src/Services/StoreDataProvider.php
+++ src/Services/StoreDataProvider.php
@@ -12,7 +12,7 @@ use Shopware\Core\Framework\Store\Struct\ReviewSummaryStruct;
 
 class StoreDataProvider
 {
-    public const HEADER_NAME_TOTAL_COUNT = 'SW-Meta-Total';
+    public const HEADER_NAME_TOTAL_COUNT = 'sw-meta-total';
 
     private StoreClient $client;
     private ExtensionLoader $extensionLoader;
@@ -50,7 +50,7 @@ class StoreDataProvider
         $listingResponse = $this->client->listExtensions($criteria, $context);
         $extensionListing = $this->extensionLoader->loadFromListingArray($context, $listingResponse['data']);
 
-        $total = $listingResponse['headers'][self::HEADER_NAME_TOTAL_COUNT][0] ?? 0;
+        $total = \array_change_key_case($listingResponse['headers'])[self::HEADER_NAME_TOTAL_COUNT][0] ?? 0;
         $extensionListing->setTotal((int) $total);
 
         return $extensionListing;
