12c079e444c3649e468d39044d95a34acdbe2bc5
Fabian Boensch <f.boensch@shopware.com>
2025-06-24 08:49:08 +0200

    comma

--- src/Resources/app/administration/src/module/sw-extension-store/component/sw-extension-store-accessibility/index.js
+++ src/Resources/app/administration/src/module/sw-extension-store/component/sw-extension-store-accessibility/index.js
@@ -2,5 +2,5 @@ import template from './sw-extension-store-accessibility.html.twig';
 import './sw-extension-store-accessibility.scss';
 
 export default Shopware.Component.wrapComponentConfig({
-    template,
+    template
 });
