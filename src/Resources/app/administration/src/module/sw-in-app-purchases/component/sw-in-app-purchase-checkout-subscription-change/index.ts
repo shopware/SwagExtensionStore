d2717a8d4355210b51502d2749bba5441b061648
Jannik <43863063+qysp@users.noreply.github.com>
2026-04-07 12:03:16 +0200

    ECTO-2519 - improve IAP checkout subscription change UI (#137) (#141)

--- src/Resources/app/administration/src/module/sw-in-app-purchases/component/sw-in-app-purchase-checkout-subscription-change/index.ts
+++ src/Resources/app/administration/src/module/sw-in-app-purchases/component/sw-in-app-purchase-checkout-subscription-change/index.ts
@@ -28,50 +28,96 @@ export default Shopware.Component.wrapComponentConfig({
             return Shopware.Filter.getByName('currency');
         },
 
-        formattedStartingDate(): string {
-            const date = new Date(this.cartPosition.nextBookingDate ?? '');
-            return date.toLocaleDateString(this.locale, { month: 'numeric', day: 'numeric' });
+        isNextBookingDateInDifferentYear(): boolean {
+            const today = new Date();
+            const nextBookingDate = this.cartPosition?.nextBookingDate;
+            return nextBookingDate !== null && new Date(nextBookingDate).getFullYear() !== today.getFullYear();
         },
 
-        infoHint(): string {
-            const today = new Date().toLocaleDateString(this.locale, {
-                month: 'long',
-                day: 'numeric'
-            });
+        yearFormat(): 'numeric' | '2-digit' | undefined {
+            return this.isNextBookingDateInDifferentYear ? 'numeric' : undefined;
+        },
 
-            const nextBookingDate = this.cartPosition?.nextBookingDate
+        infoHint(): string | null {
+            if (!this.cartPosition) {
+                return null;
+            }
+
+            const variant = this.$t(`sw-in-app-purchase-checkout-subscription-change.variant.${this.cartPosition.variant}`);
+
+            const nextBookingDate = this.cartPosition.nextBookingDate
                 ? new Date(this.cartPosition.nextBookingDate).toLocaleDateString(this.locale, {
                     month: 'long',
-                    day: 'numeric'
+                    day: 'numeric',
+                    year: this.yearFormat
                 })
                 : '';
 
-            return this.$t('sw-in-app-purchase-checkout-subscription-change.info-lint', {
+            if (this.cartPosition.subscriptionChange?.type === 'downgrade') {
+                return this.$t('sw-in-app-purchase-checkout-subscription-change.downgrade-hint', {
+                    variant,
+                    fee: this.currencyFilter(this.cart.netPrice, 'EUR', 2),
+                    start: nextBookingDate
+                });
+            }
+
+            const today = new Date().toLocaleDateString(this.locale, {
+                month: 'long',
+                day: 'numeric',
+                year: this.yearFormat
+            });
+
+            return this.$t('sw-in-app-purchase-checkout-subscription-change.upgrade-hint', {
                 today,
-                price: this.currencyFilter(this.cartPosition?.proratedNetPrice, 'EUR', 2),
-                variant: this.cartPosition?.variant ?? '',
+                price: this.currencyFilter(this.cartPosition.proratedNetPrice, 'EUR', 2),
+                variant,
                 fee: this.currencyFilter(this.cart.netPrice, 'EUR', 2),
                 start: nextBookingDate
             });
         },
 
-        cartPosition() {
+        cartPosition(): IAP.InAppPurchaseCartPosition {
             return this.cart.positions[0];
         },
 
-        isIncludedInPluginLicense() {
+        isIncludedInPluginLicense(): boolean {
             return this.cartPosition?.subscriptionChange?.isIncludedInPluginLicense ?? false;
         },
 
-        getCurrentPrice() {
-            const price = this.cartPosition?.subscriptionChange?.currentFeature?.priceModels
-                ?.find((priceModel) => priceModel.variant === this.cartPosition.variant)?.price;
-
-            return String(this.currencyFilter(price, 'EUR', 2));
+        currentPrice(): string {
+            return String(this.currencyFilter(this.cartPosition?.subscriptionChange?.currentNetPrice, 'EUR', 2));
         },
 
-        getCurrentPlanName() {
-            return this.cartPosition?.subscriptionChange?.currentFeature?.name;
+        currentPlanName(): string {
+            return this.cartPosition?.subscriptionChange?.currentFeature.name ?? '';
+        },
+
+        currentPlanDuration(): string | null {
+            if (!this.cartPosition?.subscriptionChange?.currentFeatureVariant) {
+                return null;
+            }
+
+            return this.$t(`sw-in-app-purchase-price-box.duration.${this.cartPosition?.subscriptionChange?.currentFeatureVariant}`);
+        },
+
+        newPlanName(): string {
+            return this.cartPosition?.feature.name ?? '';
+        },
+
+        newPlanDuration(): string | null {
+            if (!this.cartPosition?.variant) {
+                return null;
+            }
+
+            return this.$t(`sw-in-app-purchase-price-box.duration.${this.cartPosition?.variant}`);
+        },
+
+        proratedNetPrice(): string {
+            if (this.cartPosition?.subscriptionChange?.type === 'downgrade') {
+                return String(this.currencyFilter(0.0, 'EUR', 2));
+            }
+
+            return String(this.currencyFilter(this.cartPosition?.proratedNetPrice, 'EUR', 2));
         }
     }
 });
