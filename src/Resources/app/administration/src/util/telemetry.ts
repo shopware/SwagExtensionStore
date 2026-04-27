b00f844a4d3bf3fabd870f11069d702299a76bd0
Jannik <43863063+qysp@users.noreply.github.com>
2026-04-27 10:10:34 +0200

    ECTO-2581 - Add tracking for extension store checkout (#144)

---
+++ src/Resources/app/administration/src/util/telemetry.ts
@@ -0,0 +1,29 @@
+import type { TrackableType } from 'src/core/telemetry/types';
+import extensionStoreContextStore
+    from "SwagExtensionStore/module/sw-extension-store/store/extension-store-context.store";
+
+const collectDefaultEventData = (): Record<string, TrackableType> => {
+    const eventData: Record<string, TrackableType> = {
+        source: 'SwagExtensionStore',
+        extension_store_version: __SWAG_EXTENSION_STORE_VERSION__,
+    };
+
+    const contextStore = extensionStoreContextStore();
+    if (contextStore.skyBridgeStoreVersion !== null) {
+        eventData['sky_bridge_store_version'] = contextStore.skyBridgeStoreVersion;
+    }
+
+    return eventData;
+};
+
+export const trackExtensionStoreEvent = (eventName: string, data: Record<string, TrackableType> = {}): void => {
+    if (!('Telemetry' in Shopware)) {
+        return;
+    }
+
+    Shopware.Telemetry.track({
+        ...data,
+        ...collectDefaultEventData(),
+        eventName,
+    });
+};
