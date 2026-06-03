f19f83f5974a93c875d8e619a9fb7a826709798f
Jannik <43863063+qysp@users.noreply.github.com>
2026-06-03 08:40:44 +0200

    ECTO-2672 - Remove unused components, snippets, endpoints etc. (#161)

--- src/Resources/app/administration/src/module/sw-extension-store/index.js
+++ src/Resources/app/administration/src/module/sw-extension-store/index.js
@@ -1,5 +1,3 @@
-import ExtensionStoreService from './service/extension-store.service';
-import ExtensionStoreDataService from './service/extension-store-data.service';
 import ExtensionLicenseService from './service/extension-store-licenses.service';
 import {
     ExtensionStoreChannelService,
@@ -9,30 +7,11 @@ import extensionStoreContextStore from './store/extension-store-context.store';
 
 
 Shopware.Component.register('sw-extension-store-index', () => import('./page/sw-extension-store-index'));
-Shopware.Component.register('sw-extension-store-slider', () => import('./component/sw-extension-store-slider'));
-Shopware.Component.register('sw-extension-store-listing-filter', () => import('./component/sw-extension-store-listing-filter'));
-Shopware.Component.register('sw-extension-buy-modal', () => import('./component/sw-extension-buy-modal'));
 Shopware.Component.register('sw-extension-store-purchase-confirmation-checkout', () => import('./component/sw-extension-store-purchase-confirmation-checkout'));
 Shopware.Component.register('sw-extension-store-purchase-confirmation-checkout-overview', () => import('./component/sw-extension-store-purchase-confirmation-checkout-overview'));
 Shopware.Component.register('sw-extension-store-purchase-confirmation-modal', () => import('./component/sw-extension-store-purchase-confirmation-modal'));
 Shopware.Component.register('sw-extension-store-purchase-confirmation-permissions', () => import('./component/sw-extension-store-purchase-confirmation-permissions'));
-Shopware.Component.register('sw-extension-listing-card', () => import('./component/sw-extension-listing-card'));
-Shopware.Component.register('sw-extension-store-update-warning', () => import('./component/sw-extension-store-update-warning'));
-Shopware.Component.register('sw-extension-label', () => import('./component/sw-extension-store-label'));
-Shopware.Component.register('sw-extension-type-label', () => import('./component/sw-extension-store-type-label'));
-Shopware.Component.register('sw-extension-store-label-display', () => import('./component/sw-extension-store-label-display'));
-Shopware.Component.register('sw-extension-store-error-card', () => import('./component/sw-extension-store-error-card'));
-Shopware.Component.register('sw-extension-store-accessibility', () => import('./component/sw-extension-store-accessibility'));
-Shopware.Component.register('sw-extension-store-statistics-promotion', () => import('./component/sw-extension-store-statistics-promotion'));
 Shopware.Component.register('sw-extension-store-in-app-purchases-listing-modal', () => import('./component/sw-extension-store-in-app-purchases-listing-modal'));
-
-
-Shopware.Application.addServiceProvider('extensionStoreService', () => {
-    return new ExtensionStoreService(
-        Shopware.Service('shopwareDiscountCampaignService'),
-        Shopware.Service('shopwareExtensionService'),
-    );
-});
 
 Shopware.Application.addServiceProvider('extensionStoreChannelService', () => {
     return new ExtensionStoreChannelService(
@@ -43,13 +22,6 @@ Shopware.Application.addServiceProvider('extensionStoreChannelService', () => {
         Shopware.Service('cacheApiService'),
         Shopware.Application.view.router,
         Shopware.Service('extensionStorePreferencesService'),
-    );
-});
-
-Shopware.Application.addServiceProvider('extensionStoreDataService', () => {
-    return new ExtensionStoreDataService(
-        Shopware.Application.getContainer('init').httpClient,
-        Shopware.Service('loginService'),
     );
 });
 
@@ -79,6 +51,21 @@ Shopware.Module.register('sw-extension-store', {
                 privilege: 'system.extension_store',
             },
             component: 'sw-extension-store-index',
+        },
+        // The Shopware Admin requires this exact route name (sw.extension.store.detail) to link to the details page in the Store.
+        'store.detail': {
+            path: 'store/detail/:id',
+            meta: {
+                parentPath: 'sw.extension.store',
+                privilege: 'system.extension_store',
+            },
+            // TODO(ECTO-2679): Implement deep linking to PDP.
+            component: 'sw-extension-store-index',
+            props: {
+                default: (route) => {
+                    return { id: route.params.id };
+                },
+            },
         },
     },
 
