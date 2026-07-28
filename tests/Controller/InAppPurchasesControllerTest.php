272c3ad6951684b7e0656a313e7deb6031a72b95
RoboShyim <robo@shyim.de>
2026-07-28 06:37:59 +0200

    fix: unbreak PHPStan EntitySearchResult deprecations (#189)
    Co-authored-by: roboshyim <roboshyim@users.noreply.github.com>

--- tests/Controller/InAppPurchasesControllerTest.php
+++ tests/Controller/InAppPurchasesControllerTest.php
@@ -452,7 +452,7 @@ class InAppPurchasesControllerTest extends TestCase
         $app->setUniqueIdentifier(Uuid::randomHex());
 
         return new EntitySearchResult(
-            'aoo',
+            'app',
             1,
             new EntityCollection([$app]),
             null,
