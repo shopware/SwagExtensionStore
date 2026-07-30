72ed6ad3127393fa3f0f5ce44cef9f5364373054
Michael Telgmann <mitelg@users.noreply.github.com>
2026-07-30 16:08:09 +0200

    chore: Prepare code for PHPStan update 2026-07 (#192)

--- src/Struct/InAppPurchaseCartPositionStruct.php
+++ src/Struct/InAppPurchaseCartPositionStruct.php
@@ -12,8 +12,45 @@ use Shopware\Core\Framework\Struct\Struct;
  * @phpstan-import-type InAppPurchasePriceModel from InAppPurchasePriceModelStruct
  * @phpstan-import-type InAppPurchaseSubscriptionChange from InAppPurchaseSubscriptionChangeStruct
  *
- * @phpstan-type InAppPurchaseCartPosition array{subscriptionChange?: InAppPurchaseSubscriptionChange, extensionName: string, inAppFeatureIdentifier: string, netPrice: float, grossPrice: float, taxRate: float, taxValue: float}
- * @phpstan-type InAppPurchaseCartItem array{extensionName: string, inAppFeatureIdentifier: string, netPrice: float, taxValue: float, grossPrice: float, taxRate: float, variant: string, subscriptionChange?: array{type: string, currentInAppFeatureIdentifier: string}}
+ * @phpstan-type InAppPurchaseCartPosition array{
+ *     subscriptionChange?: InAppPurchaseSubscriptionChange,
+ *     extensionName: string,
+ *     inAppFeatureIdentifier: string,
+ *     priceModel?: array{
+ *         type: string,
+ *         price: float,
+ *         duration: int|null,
+ *         oneTimeOnly: bool,
+ *     },
+ *     feature?: array{
+ *         name: string,
+ *         identifier: string,
+ *         description: string|null,
+ *         priceModels: array{
+ *             type: string,
+ *             price: float,
+ *             duration: int|null,
+ *             oneTimeOnly: bool,
+ *         },
+ *     },
+ *     netPrice: float,
+ *     grossPrice: float,
+ *     taxRate: float,
+ *     taxValue: float,
+ * }
+ * @phpstan-type InAppPurchaseCartItem array{
+ *     extensionName: string,
+ *     inAppFeatureIdentifier: string,
+ *     netPrice: float,
+ *     taxValue: float,
+ *     grossPrice: float,
+ *     taxRate: float,
+ *     variant: string,
+ *     subscriptionChange?: array{
+ *         type: string,
+ *         currentInAppFeatureIdentifier: string
+ *     }
+ * }
  */
 #[Package('checkout')]
 class InAppPurchaseCartPositionStruct extends Struct
